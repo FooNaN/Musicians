37e5a0b6ff21c06c5e9c48bf48a848f0ac568ad6
xalvaine <xalvaine@gmail.com>
2021-03-18 23:16:07 +0300

    feature: connect to db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,19 +2,21 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { VenuesModule } from './venues/venues.module'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: `postgres`,
-      host: `localhost`,
+      host: `database`,
       port: 5432,
-      username: `root`,
-      password: `root`,
-      database: `test`,
+      username: `xalvaine`,
+      password: `example`,
+      database: `musicians`,
       entities: [],
       synchronize: true,
     }),
+    VenuesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
